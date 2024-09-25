417b39f6b71992d9153a5b88cf3d6a3a593462be
Maksym Horokhov <maksym.horokhov@guest.hasna.com>
2024-09-25 13:42:58 +0300

    fixes for result saving

--- coin-service/src/index.ts
+++ coin-service/src/index.ts
@@ -1,13 +1,13 @@
 import * as cron from "cron";
 import express, {json} from 'express';
-import {MainJob, result} from "./job.js";
+import {mainJob} from "./job.js";
 
-const orderBookUpdateJob = new cron.CronJob('*/30 * * * * *', MainJob);
+const orderBookUpdateJob = new cron.CronJob('*/30 * * * * *', mainJob.job.bind(mainJob));
 orderBookUpdateJob.start();
 
 const app = express();
 app.use(json());
-app.get('', (req, res) => res.status(200).json(result))
+app.get('', (req, res) => res.status(200).json(mainJob.result))
 const port = process.env.PORT;
 // Start the server
 app.listen(port, async () => {
--- coin-service/src/services/main.service.ts
+++ coin-service/src/services/main.service.ts
@@ -1,11 +1,9 @@
 import {fetchOrderBook} from "../utils/ccxt.js";
 
 export class MainService {
-  data: any[] = []
 
   async updateOrderBook(coinSlug: string, marketPair: string, exchangeSlug: string) {
-    const orderBook = await fetchOrderBook(exchangeSlug, marketPair);
-    this.data.push(orderBook)
+    return await fetchOrderBook(exchangeSlug, marketPair);
   }
 }
 
